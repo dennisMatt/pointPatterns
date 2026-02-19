22547be0e62a5d6df65814575eab37a723453483
dennisMatt <matthew.dennis@manchester.ac.uk>
2026-02-19 16:25:34 +0000

    set title and load packages

---
+++ ripleysK.R
@@ -0,0 +1,9 @@
+#===================================Point patterns with Ripley's K====================
+#This script creates two point patterns, one clusters and one unifor and builds
+#a function to compute and plot Ripley's K for each
+
+#load spatstat,sf and ggplot
+
+library(spatstat)
+library(ggplot2)
+library(sf)
