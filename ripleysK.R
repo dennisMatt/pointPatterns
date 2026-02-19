768170ed6ef5ad6172176ef97d53e80b5fbb1e51
dennisMatt <matthew.dennis@manchester.ac.uk>
2026-02-19 16:26:17 +0000

    Create clustered point pattern

--- ripleysK.R
+++ ripleysK.R
@@ -7,3 +7,19 @@
 library(spatstat)
 library(ggplot2)
 library(sf)
+
+#First build a clustered point pattern. We can do this simply by generating random x and y coordinates
+#drawn from the same normal distribution for each (this means tha values for x and y will tend towards the mean so we can be sure of a clumped patter)
+
+#set seed value for reproduceability
+set.seed(9)
+x <- rnorm(100, 100, 30)
+y <- rnorm(100, 100, 30)
+
+#create point pattern within a 200 by 200 unit square
+dummyPoints <- ppp(x = x, y = y, window = square(200))
+
+plot(dummyPoints)
+points=dummyPoints
+
+r=1:100
